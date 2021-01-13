4112ff689bee3f14d2c13d215f644f627359fcde
pablo <you@example.com>
2021-01-13 15:34:46 -0300

    incorporando logica core

---
+++ recicla/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ReciclaConfig(AppConfig):
+    name = 'recicla'
---
+++ recicla/entities.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class User:
+    id: str
+    name: str
+    email: str
+reci
